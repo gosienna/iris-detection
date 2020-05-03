4ce75b3e83e6f0ea37d5fdd437af2841cfde3236
gosienna <gosienna@gmail.com>
2020-05-03 08:51:01 +0800

    aaa

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 
 app.use(express.static("./static"));
-const PORT=process.env.PORT||5000;
+const PORT=process.env.PORT||81;
 app.listen(PORT, function() {
     console.log("Listening on port 81");
 });
